d145510ccf6d6160a8af1b1bf603ccb68d9f44b1
benwhite0820 <dragon7230@gmail.com>
2023-07-13 16:54:48 +0800

    typescript practice

--- src/App.js
+++ src/App.js
@@ -11,22 +11,22 @@ import Checkout from './routes/checkout/checkout.component';
 import { checkUserSession } from './store/user/user.action';
 
 const App = () => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(checkUserSession());
-  }, []);
+    useEffect(() => {
+        dispatch(checkUserSession());
+    }, []);
 
-  return (
-    <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path='shop/*' element={<Shop />} />
-        <Route path='auth' element={<Authentication />} />
-        <Route path='checkout' element={<Checkout />} />
-      </Route>
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Navigation />}>
+                <Route index element={<Home />} />
+                <Route path="shop/*" element={<Shop />} />
+                <Route path="auth" element={<Authentication />} />
+                <Route path="checkout" element={<Checkout />} />
+            </Route>
+        </Routes>
+    );
 };
 
 export default App;
--- src/store/categories/category.action.ts
+++ src/store/categories/category.action.ts
@@ -1,37 +1,37 @@
 import { CATEGORIES_ACTION_TYPES, Category } from './category.types';
 
 import {
-  createAction,
-  Action,
-  withMatcher,
-  ActionWithPayload,
+    createAction,
+    Action,
+    withMatcher,
+    ActionWithPayload,
 } from '../../utils/reducer/reducer.utils';
 
 export type FetchCategoriesStart =
-  Action<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START>;
+    Action<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START>;
 
 export type FetchCategoriesSuccess = ActionWithPayload<
-  CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
-  Category[]
+    CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+    Category[]
 >;
 
 export type FetchCategoriesFailed = ActionWithPayload<
-  CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED,
-  Error
+    CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED,
+    Error
 >;
 
 export const fetchCategoriesStart = withMatcher(() =>
-  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START)
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START)
 );
 
 export const fetchCategoriesSuccess = withMatcher(
-  (categoriesArray: Category[]) =>
-    createAction(
-      CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
-      categoriesArray
-    )
+    (categoriesArray: Category[]) =>
+        createAction(
+            CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+            categoriesArray
+        )
 );
 
 export const fetchCategoriesFailed = withMatcher((error: Error) =>
-  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error)
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error)
 );
